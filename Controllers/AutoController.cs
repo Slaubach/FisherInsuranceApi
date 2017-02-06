47b8d09c1f7a81dc1b72536854891d3e72ccfc12
Slaubach <launch.17@osu.edu>
2017-02-06 15:17:26 -0500

    lab2

---
+++ Controllers/AutoController.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+[Route("api/auto/quotes")]
+    
+
+    public class AutoController : Controller
+    {
+    [HttpPost]
+    // GET: /<controller>/
+    public IActionResult Post([FromBody]string value)
+    {
+        return Created("", value);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        return Ok("The id is: " + id);
+    }
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody]string value)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        return Delete(id);
+    }
+}
+
---
+++ Controllers/ClaimsController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+
+    public class ClaimsController : Controller
+    {
+        // GET: /<controller>/
+       
+    [HttpPost]
+        // GET: /<controller>/
+        public IActionResult Post([FromBody]string value)
+        {
+            return Created("", value);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok("The id is: " + id);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]string value)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Delete(id);
+        }
+    }
